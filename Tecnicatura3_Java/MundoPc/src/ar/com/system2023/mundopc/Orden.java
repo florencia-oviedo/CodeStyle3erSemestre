0da261a068cd2ab4b1c279318ccd0781deafc5a3
ivanagermir <germirivana@gmail.com>
2023-04-21 15:32:29 -0300

    Ultimos cambios

--- Tecnicatura3_Java/MundoPc/src/ar/com/system2023/mundopc/Orden.java
+++ Tecnicatura3_Java/MundoPc/src/ar/com/system2023/mundopc/Orden.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ar.com.system2023.mundopc;
 
-/**
- *
- * @author 01
- */
+
 public class Orden {
+    private final int idOrden;
+    private Computadora computadora[]; //Inicializamos un arreglo de objetos vacío
+    private static int contadorOrdenes;
+    private static final int MAX_COMPUTADORAS =10;
+    private int contadorComputadora;
     
+    //Creamos el constructor vacío
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.computadora = new Computadora[Orden.MAX_COMPUTADORAS];
+    }
+    
+    // Creamos el método para agregar una nueva computadora al arreglo
+    public void agregarComputadora(Computadora computadora){
+        if(this.contadorComputadora < Orden.MAX_COMPUTADORAS){
+            this.computadora[this.contadorComputadora++] = computadora;
+        }
+        else{
+            System.out.println("Has superado el limite: "+ Orden.MAX_COMPUTADORAS);
+        }
+    }
+    
+    //Método mostrar orden
+    public void mostrarOrden(){
+        System.out.println("Orden #: "+ this.idOrden);
+        System.out.println("Computadoras de la orden #: "+this.idOrden);
+        for(int i = 0; i < this.contadorComputadora; i++){
+            System.out.println(this.computadora[i]);
+        }
+    }
 }
