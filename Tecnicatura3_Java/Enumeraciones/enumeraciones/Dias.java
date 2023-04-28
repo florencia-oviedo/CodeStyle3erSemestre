d8d2f07895ac069fecd877e74b764a920fc29512
Adriana Soledad Da Silva <adrianasilva523@gmail.com>
2023-04-28 19:10:41 -0300

    Clases Dias y Continentes

---
+++ Tecnicatura3_Java/Enumeraciones/enumeraciones/Dias.java
@@ -0,0 +1,14 @@
+
+package enumeraciones;
+
+public enum Dias {
+    //es una clase particular que contiene varios elementos
+    //sus elementos son constantes (public static final)
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO
+}
