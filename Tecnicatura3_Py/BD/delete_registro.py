5da3140262c38ec0fe0f886069bc90ab4f7808be
Adriana Soledad Da Silva <adrianasilva523@gmail.com>
2023-05-17 19:19:33 -0300

    Corrigiendo el archivo delete_registro.py

--- Tecnicatura3_Py/BD/delete_registro.py
+++ Tecnicatura3_Py/BD/delete_registro.py
@@ -4,9 +4,9 @@ conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1',
 try:
     with conexion:
         with conexion.cursor() as cursor:
-            sentencia = 'DELETE FROM persona WHERE id_persona IN %s'
+            sentencia = 'DELETE FROM persona WHERE id_persona=%s'
             entrada = input('Digite los números de registros a eliminar (separados por coma): ')
-            valores = (tuple(entrada.split(',')),)  # es una tupla de tuplas
+            valores = (entrada,)  # es una tupla de valores
             cursor.execute(sentencia, valores)
             registros_eliminados = cursor.rowcount
             print(f'Los registros eliminados son: {registros_eliminados}')
