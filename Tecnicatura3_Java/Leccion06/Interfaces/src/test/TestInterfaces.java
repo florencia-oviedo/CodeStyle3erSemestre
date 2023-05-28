89fd32591708acf89737dcd0f9bd7724ddee0fd4
ivanagermir <germirivana@gmail.com>
2023-05-28 17:23:33 -0300

    Agrego paquete Test en carpeta Leccion06

---
+++ Tecnicatura3_Java/Leccion06/Interfaces/src/test/TestInterfaces.java
@@ -0,0 +1,17 @@
+package test;
+
+import accesodatos.*;
+
+public class TestInterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMySql();
+        
+        //datos.listar();
+        imprimir(datos);
+        datos = new ImplementacionOracle();
+        //datos.listar();
+    }
+    public static void imprimir(IAccesoDatos datos){
+        datos.listar();
+    }
+}
