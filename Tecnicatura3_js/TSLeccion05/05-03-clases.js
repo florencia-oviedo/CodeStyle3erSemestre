d8a70dd456ecaf69356367d18c6f91fcc4fb2c05
gabi92708 <emegabi88@gmail.com>
2023-06-05 00:04:51 -0300

    Agregué al archivo 05-03-clases.js el método static saludar, el método saludar2 y el atributo no estático como en los videos 8.1, 8.2 y 8.3

--- Tecnicatura3_js/TSLeccion05/05-03-clases.js
+++ Tecnicatura3_js/TSLeccion05/05-03-clases.js
@@ -1,9 +1,15 @@
 //let persona3 = new Persona("Carla", "Ponce");
 // no se puede crear un objeto antes de que esté la clase creada
 class Persona{  //Clase padre
+
+    static contadorObjetosPersona = 0; //Atributo estático
+    email = "Valor default email"; //Atributo no estático
+
     constructor(nombre, apellido){
         this._nombre = nombre;
         this._apellido = apellido;
+        Persona.contadorObjetosPersona++;
+        console.log("Se incrementa el contador: "+Persona.contadorObjetosPersona);
     }
 
     get nombre(){
@@ -22,6 +28,24 @@ class Persona{  //Clase padre
         this._apellido = apellido;
     }
 
+    nombreCompleto(){
+        return this._nombre+" "+this._apellido;
+    }
+
+    //Sobreescribiendo el método de la clase padre (Object)
+    toString(){  //Regresa un String
+        //Se aplica el polimorfismo que significa = multiples formas en tiempo de ejecución
+        //El método que se ejecuta depende si es una referencia de tipo padre o hija
+        return this.nombreCompleto();
+    }
+    
+    static saludar(){
+        console.log("Saludos desde este método static");
+    }
+
+    static saludar2(persona){
+        console.log(persona.nombre+" "+persona.apellido);
+    }
 }
 
 class Empleado extends Persona{ //Clase hija
@@ -36,6 +60,11 @@ class Empleado extends Persona{ //Clase hija
 
     set departamento(departamento){
         this.departamento = departamento;
+    }
+
+    //Sobreescritura
+    nombreCompleto(){
+        return super.nombreCompleto()+", "+this._departamento;
     }
 }
 
@@ -59,4 +88,20 @@ console.log(persona2.apellido);
 
 let empleado1 = new Empleado("María", "Giménez", "Sistemas");
 console.log(empleado1);
+console.log(persona1.toString());
 
+//persona1.saludar();  //no se utiliza desde el objeto, si desde la clase
+Persona.saludar();
+Persona.saludar2(persona1);
+
+Empleado.saludar();
+Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona); 
+//Las clases hijas heredadn tmb los atributos estáticos
+
+console.log(persona1.email);
+console.log(empleado1.email);
+// console.log(Persona.email); //No se puede acceder desde la clase
