9d3f1463cb42f19bc8da2d25094adcd74c2fa39a
Fernando Rojas <rojasfj@gmail.com>
2023-06-11 15:38:26 -0600

    Update conexion.py

--- Tecnicatura3_Py/Leccion07/capa_datos_persona/conexion.py
+++ Tecnicatura3_Py/Leccion07/capa_datos_persona/conexion.py
@@ -27,7 +27,22 @@ class Conexion:
         else:
             return cls._conexion
         
-
+# AGREGAR CURSOR        
+@classmethod
+def obtenerCursor(cls):
+    if cls._cursor is None:
+        try:
+            cls._cursor = cls.obtenerConexion().cursor()
+            log.debug(f'Se abrió correctmente el cursor: {cls._cursor}')
+            return cls._cursur
+        except Exception as e:
+            log.error(f'Ocurrió un error: {e}')
+            sys.exit()
+    else:
+        return cls._cursor
+if __name__ == '__main__':
+    Conexion.obtenerConexion()
+    Conexion.obtenerCursor()
     
     
 
