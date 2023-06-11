41f5f1c65c228a441cf1212f5647ba23ecf7d3f7
Florencia Micaela Oviedo <flor.oviedo2010@gmail.com>
2023-06-11 16:44:21 -0300

    Leccion09

---
+++ Tecnicatura3_Java/Leccion09/CalculadoraUTN/src/CalculadoraUTN.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("******* Aplicación Calculadora *******");
+        System.out.print("Digite el valor para el operando1: ");
+        var operando1 = Integer.parseInt(entrada.nextLine());
+        System.out.print("Digite el valor para el operando2: ");
+        var operando2 = Integer.parseInt(entrada.nextLine());
+        var resultado = operando1 + operando2;
+        System.out.println("resultado = " + resultado);
+    }
+
+}
