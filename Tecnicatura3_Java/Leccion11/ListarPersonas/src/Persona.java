0f4f5b64bae037a172c42433f30b2fb825c3dbbb
Adriana Soledad Da Silva <adrianasilva523@gmail.com>
2023-06-20 20:26:55 -0300

    Se crea proyecto Listar Personas + clase Persona y UML

--- Tecnicatura3_Java/Leccion11/ListarPersonas/src/Persona.java
+++ Tecnicatura3_Java/Leccion11/ListarPersonas/src/Persona.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Persona {
+public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    //Constructor vacio
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+
+    //Constructor con parámetros(sobrecarga de constructores)
+    public Persona(String nombre, String tel, String email){
+        this();
+        this.nombre = nombre;
+        this.tel = tel;
+        this.email = email;
+    }
+
+
 }
