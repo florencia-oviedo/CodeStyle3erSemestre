58e9f6d98e3abefc6f7411d0e428b68b417e8701
gabi92708 <emegabi88@gmail.com>
2023-06-30 00:28:09 -0300

    Ingreso package UTN.dominio con la clase Estudiante y Listado de estudiantes con la Clase EstudianteDAO

---
+++ Tecnicatura3_Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -0,0 +1,47 @@
+package UTN.datos;
+
+import UTN.dominio.Estudiante;
+
+import static UTN.conexion.Conexion.getConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudianteDAO {
+    //Método listar
+    public List<Estudiante> listar(){
+        List<Estudiante> estudiantes = new ArrayList<>();
+        // Creamos algunos objetos que son necesarios para comunicarnosa con la base de datos
+        PreparedStatement ps; // Envia la sentencia a la BD
+        ResultSet rs; // Obtenemos el resultado de la BD
+        // Creamos un objeto de tipo conexión
+        Connection con = getConnection();
+        String sql = "SELECT * FROM estudiantes2022 ORDER BY estudiantes2022";
+        try{
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                var estudiante = new Estudiante();
+                estudiante.setIdEstudiante(rs.getInt("idestudiante2022"));
+                estudiante.setNombre(rs.getString("nombre"));
+                estudiante.setApellido(rs.getString("apellido"));
+                estudiante.setTelefono(rs.getString("telefono"));
+                estudiante.setEmail(rs.getString("email"));
+                // Falta agregarlo a la lista
+                estudiantes.add(estudiante);
+            }
+        } catch (Exception e){
+            System.out.println("Ocurrió un error al seleccionar datos: "+e.getMessage());
+        }
+        finally {
+            try{
+                con.close();
+            } catch (Exception e){
+                System.out.println("Ocurrió un error al cerrar la conexión: "+e.getMessage());
+            }
+        } // fin finally
+        return estudiantes;
+    } //Fin método listar
+}
